f114fd158c997b5502b65c671ef5bd7403ea94a9
40216006 <colin_andersonx360@live.co.uk>
2018-01-09 18:19:27 +0000

    added main files for app, swiping and login/register views in progress

---
+++ app/src/main/java/com/example/colin/musifind/ChooseLoginOrRegisterActivity.java
@@ -0,0 +1,42 @@
+package com.example.colin.musifind;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginOrRegisterActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_or_register);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ChooseLoginOrRegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseLoginOrRegisterActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
+    }
+}
